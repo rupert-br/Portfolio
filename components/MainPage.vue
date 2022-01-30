8d63bc09ac4499cf88f2d547547b91e7852c5b08
Rupert Brandstaetter <rupert.brandstaetter@gmail.com>
2022-01-30 20:31:07 +0100

    added projects slider

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-2 md:px-40">
-    <div class="flex flex-col md:flex-row pt-16 pb-16 items-center">
+    <div class="flex flex-col md:flex-row pt-16 pb-16 items-center mx-auto">
       <div class="basis-1/2 text-center">
         <div class="text-5xl font-bold pb-6">
           Hello, I'm Rupert
@@ -13,20 +13,10 @@
         <div class="h-72 w-72 bg-blue-600 shadow-xl rounded-full mt-12 md:mt-0" />
       </div>
     </div>
-    <div class="text-5xl font-bold mb-8 mt-6">
-      Projects
-    </div>
-    <div class="grid grid-cols-3 gap-4 justify-items-center">
-      <div class="">01</div>
-      <div>02</div>
-      <div>03</div>
-      <div>04</div>
-      <div>05</div>
-      <div>06</div>
-      <div>07</div>
-      <div>08</div>
-      <div>09</div>
-    </div>
+
+    <h1 class="text-center text-5xl font-bold mt-20">Projekte</h1>
+    <ProjectsSlider />
+
     <div class="mt-20">
       <div class="text-5xl font-bold mb-8">
         Customers
@@ -48,7 +38,7 @@
         <input id="subject" type="text" name="subject" class="border border-blue-600">
         <div>
           <label for="w3review">Message</label>
-          <textarea id="w3review" name="w3review" class="border border-blue-600"/>
+          <textarea id="w3review" name="w3review" class="border border-blue-600" />
         </div>
         <button class="py-2 px-3 mt-4 text-white bg-blue-700 hover:bg-blue-800">
           Send message!
---
+++ components/ProjectsSlider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container flex items-center relative mt-8 mx-auto">
+    <!-- <div class="absolute shadow-xl left-0 w-14 h-14 mx-auto bg-blue-50 rounded-full flex justify-center items-center text-3xl">&larr;</div>
+    <div class="absolute shadow-xl right-0 w-14 h-14 mx-auto bg-blue-50 rounded-full flex justify-center items-center text-3xl">&rarr;</div> -->
+    <div class="overflow-x-scroll flex">
+      <div class="flex-none m-5">
+        <img class="h-64 w-auto" src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea">
+        <div class="text-3xl mt-4 font-extrabold uppercase">TEST</div>
+        <div>Lorem ipsum dolor sit amet consectetur.</div>
+      </div>
+      <div class="flex-none h-60 w-96 bg-blue-600 m-5">2</div>
+      <div class="flex-none h-60 w-96 bg-blue-600 m-5">3</div>
+      <div class="flex-none h-60 w-96 bg-blue-600 m-5">4</div>
+      <div class="flex-none h-60 w-96 bg-blue-600 m-5">5</div>
+      <div class="flex-none h-60 w-96 bg-blue-600 m-5">6</div>
+    </div>
+  </div>
+</template>

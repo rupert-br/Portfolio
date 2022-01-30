67b349896c7892a26f0e452d1fd49855e0724cc0
Rupert Brandstaetter <rupert.brandstaetter@gmail.com>
2022-01-30 22:18:19 +0100

    deploy for production

--- components/ProjectsSlider.vue
+++ components/ProjectsSlider.vue
@@ -5,16 +5,21 @@
     <div class="overflow-x-scroll flex">
       <div class="flex-none m-5">
         <img class="h-64 w-auto" src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea">
-        <div class="text-3xl mt-4 font-extrabold uppercase">
+        <div class="text-lg mt-4 text-blue-600 font-extrabold uppercase">
           TEST
         </div>
-        <div>Lorem ipsum dolor sit amet consectetur.</div>
+        <div class="mb-3 text-xl">
+          Lorem ipsum dolor sit amet consectetur.
+        </div>
+        <a href="https://chaga.yoga" class="uppercase tracking-tighter font-semibold hover:text-blue-600 transition hover:underline">
+          chaga.yoga
+        </a>
       </div>
-      <div class="flex-none h-60 w-96 bg-blue-600 m-5">
-        2
+      <div class="flex-none m-5">
+        <img class="h-64 w-auto" src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea">
       </div>
-      <div class="flex-none h-60 w-96 bg-blue-600 m-5">
-        3
+      <div class="flex-none m-5">
+        <img class="h-64 w-auto" src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea">
       </div>
       <div class="flex-none h-60 w-96 bg-blue-600 m-5">
         4

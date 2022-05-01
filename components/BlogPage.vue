49f5bbd4f48c3c1f150e14473c2e5ff13aa66885
Rupert Brandstaetter <rupert.brandstaetter@gmail.com>
2022-05-01 12:46:51 +0200

    feat: added animation on blog page

--- components/BlogPage.vue
+++ components/BlogPage.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="px-2 md:px-40 h-screen mt-24 text-4xl font-spacegrotesk dark:text-white">
+  <div id="blog-container" class="px-2 md:px-40 h-screen mt-24 text-4xl font-spacegrotesk dark:text-white">
     Erster Blog-Eintrag folgt in Kürze ...
   </div>
 </template>
+
+<script>
+import { gsap } from 'gsap'
+
+export default {
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      gsap.from('#blog-container', {
+        opacity: 0,
+        y: 200,
+        duration: 1.0,
+        delay: 1.5
+      })
+    }
+  }
+}
+</script>
